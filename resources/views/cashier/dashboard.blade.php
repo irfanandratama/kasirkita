6141657ef81335486aab71e9786ac531d24cd426
shohihul <shohihul29maksud@gmail.com>
2019-12-09 13:20:13 +0700

    delete

--- resources/views/cashier/dashboard.blade.php
+++ resources/views/cashier/dashboard.blade.php
@@ -7,7 +7,7 @@
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="card card-statistic-2">
             <div class="card-icon shadow-primary bg-primary">
-                <i class="fas fa-archive"></i>
+                <i class="fas fa-store"></i>
             </div>
             <div class="card-wrap">
                 <div class="card-header">
@@ -22,7 +22,7 @@
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="card card-statistic-2">
             <div class="card-icon shadow-primary bg-primary">
-                <i class="fas fa-dollar-sign"></i>
+                <i class="fas fa-shopping-bag"></i>
             </div>
             <div class="card-wrap">
                 <div class="card-header">
@@ -37,7 +37,7 @@
         <div class="col-lg-4 col-md-4 col-sm-12">
             <div class="card card-statistic-2">
             <div class="card-icon shadow-primary bg-primary">
-                <i class="fas fa-shopping-bag"></i>
+                <i class="fas fa-dollar-sign"></i>
             </div>
             <div class="card-wrap">
                 <div class="card-header">
